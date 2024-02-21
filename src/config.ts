2f8995e430d5bc4d21a7ec0f9973db7a431d9a79
Kurt Medley <kmedley@rei.com>
2024-02-21 14:34:15 -0800

    Configuration for heavymedl.dev domain

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://www.heavymedl.dev", // replace this with your deployed domain
+  website: "https://heavymedl.dev", // replace this with your deployed domain
   author: "Kurt Medley",
   desc: "Kurt Medley is a full-stack software engineer based in Seattle, WA.",
   title: "heavymedl.dev",

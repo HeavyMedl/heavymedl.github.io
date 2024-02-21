8680e0f38965eed0161422eac61aec928d24fe97
Kurt Medley <kmedley@rei.com>
2024-02-21 11:50:16 -0800

    feat(deploy.yml): adding deploy file for github pages

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://heavymedl.github.io", // replace this with your deployed domain
   author: "Kurt Medley",
   desc: "Kurt Medley is a full-stack engineer based in Seattle, WA.",
   title: "heavymedl.dev",

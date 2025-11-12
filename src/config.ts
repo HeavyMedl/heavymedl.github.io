d06a6d02006662a8f2358f6102b93da5c7e6af62
Kurt Medley <kmedley@rei.com>
2025-11-12 13:27:47 -0800

    chore: configuration and adding logo/favicon

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export const themeConfig: ThemeConfig = {
   // SITE INFO ///////////////////////////////////////////////////////////////////////////////////////////
   site: {
     website: 'https://astro-chiri.netlify.app/', // Site domain
-    title: 'CHIRI', // Site title
-    author: '3ASH', // Author name
-    description: 'Minimal blog built by Astro', // Site description
+    title: 'heavymedl.dev', // Site title
+    author: 'HeavyMedl', // Author name
+    description: 'Brain dump site of Kurt Medley', // Site description
     language: 'en-US' // Default language
   },
 
